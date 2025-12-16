56509b2bff831f1d315810f69f4f212c2ee1b08f
aymen bouguerra <aymen.bouguerra.95@gmail.com>
2025-12-16 18:43:22 +0100

    add docker compoe

--- app/app.py
+++ app/app.py
@@ -1,22 +1,25 @@
-from flask import Flask, jsonify
-import psutil 
+from flask import Flask, Response
+from prometheus_client import Gauge, generate_latest, CONTENT_TYPE_LATEST
+import psutil
 
 app = Flask(__name__)
+
+cpu_gauge = Gauge('cpu_usage_percent', 'CPU usage percent')
+memory_gauge = Gauge('memory_usage_percent', 'Memory usage percent')
+
 @app.route("/")
 def home():
-    return " monotoring tool is running"
-@app.route("/status")
-def status():
-    return {"status":'ok',"service": 'monotoring cpu', "version":'1.0.0'}    
-@app.route("/health")
-def health():
-    data ={
-        "cpu_usage": psutil.cpu_percent(interval=1),
-        "memory": psutil.virtual_memory()._asdict(),
-        "disk": psutil.disk_usage('/')._asdict(),
-        "boot_time": psutil.boot_time()
-    }
-    return jsonify(data)
+    return "Monitoring Tool is running"
+
+@app.route("/metrics")
+def metrics():
+    cpu_gauge.set(psutil.cpu_percent(interval=1))
+    memory_gauge.set(psutil.virtual_memory().percent)
+
+    return Response(
+        generate_latest(),
+        mimetype=CONTENT_TYPE_LATEST
+    )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000)
